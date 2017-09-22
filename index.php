3e543987d542d71c4b526d6623d6b9ef0ce52eed
Nguyen Hai <hainguyen4391@gmail.com>
2017-09-22 21:38:21 +0700

    + add file

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="row">
+        <div class="col-lg-16">
+            <nav class="navigation">
+                <ul>
+                    <li>Home</li>
+                    <li>
+                        <a href="">Men</a>
+                        <ul class="menu">
+                            <li>
+                                <a href="">Ao nam</a>
+                                <ul>
+                                    <li><a href="">Ao khoac</a></li>
+                                    <li><a href=""></a></li>
+                                    <li><a href=""></a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Quan nam</a>
+                                <ul>
+                                    <li><a href="">Quan tay</a></li>
+                                    <li><a href="">Quan jean</a></li>
+                                    <li><a href="">Quan dui</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">T-shirt</a>
+                                
+                            </li>
+                            <li>
+                                <a href="">So-mi</a>
+                                
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Women</a>
+                    </li>
+                    <li>
+                        <a href="">New Products</a>
+                    </li>
+                    <li>
+                        <a href="">Hot Sales</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</body>
+</html>
