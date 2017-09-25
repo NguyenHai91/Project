f472852e3924d9c0403d6bde3623ac5852faa43d
Nguyen Hai <hainguyen4391@gmail.com>
2017-09-25 11:28:21 +0700

    @

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ window.addEventListener('DOMContentLoaded', function(event) {
     var index = -1;
     var imageBox = document.getElementById('image-box');
     var listImage = imageBox.querySelector('.list-image');
+    var prev = document.getElementById('prev');
+    var next = document.getElementById('next');
 
     function handerPositionImage(n) {
         index = index + n;
@@ -15,6 +17,12 @@ window.addEventListener('DOMContentLoaded', function(event) {
         }
     }
 
+    function plusIndex(n) {
+        handerPositionImage(n);
+        listImage.style.transition = 'transform 0.2s ease';
+        listImage.style.transform = 'translateX(' + index * -1200 + 'px)';
+    }
+
     function slideImage(n) {
         handerPositionImage(n);
         listImage.style.transition = 'transform 1s ease';
@@ -25,5 +33,12 @@ window.addEventListener('DOMContentLoaded', function(event) {
         slideImage(1);
         setTimeout(controlSlide, 6000);
     }
+    prev.addEventListener('click', function() {
+        plusIndex(-1);
+    });
+    next.addEventListener('click', function() {
+        plusIndex(1);
+    });
     controlSlide();
+
 });
